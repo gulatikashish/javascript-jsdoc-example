7a2fc7ca8bacdd4d9f6d9b08f2abf677b344bab2
Kashish <kashish.gulati@walkwel.in>
2018-02-28 17:27:10 +0530

    source code

---
+++ func.js
@@ -0,0 +1,53 @@
+/**
+ *sum .
+ * @constructor
+ * @description description-sum of 2 numbers]
+ * @summary summary-sum of 2 numbers
+ * @requires jsdoc
+ * @param {number} a - first number
+ * @param {numbet} b - second number
+ * @example 1+2=3
+ * @public
+ * @returns sum of 2 numbers
+ */
+function sum(a, b) {
+  return a + b
+}
+
+/**
+*multiply .
+
+* @param {number} a - first number
+* @param {numbet} b - second number
+* @example 2*3=6
+* @public
+* @returns multiplication of 2 numbers
+*/
+function multiply(a, b) {
+  return a * b
+}
+/**
+*divide .
+
+* @param {number} a - first number
+* @param {numbet} b - second number
+* @example 4/2=2
+* @private
+* @returns division of 2 numbers
+*/
+function division(a, b) {
+  return a / b
+}
+
+/**
+ * subtract .
+ 
+ * @param {number} a - first number
+ * @param {numbet} b - second number
+ * @example 4-2=2
+ * @public
+ * @returns subraction of 2 numbers
+ */
+function subtract(a, b) {
+  return a - b
+}
